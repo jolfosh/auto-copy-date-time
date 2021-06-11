9e76250a14bd48ce37dca624a8676c16c75f7515
Josh Chasteen <82539227+jolfosh@users.noreply.github.com>
2021-06-11 09:49:49 -0500

    Added status messages

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import time
 def add_to_clipboard():
     time = '{0:%m-%d %H:%M}'.format(datetime.datetime.now())
     pyperclip.copy(time)
+    print(time)
     
 
 if __name__ == '__main__':
+    print("Starting Script...")
     try:
         add_to_clipboard()
         second_time = int(time.time())
         delta_minute = 60 - (second_time % 60)
+        print("Script Running...")
         time.sleep(delta_minute)
         
         while True:
